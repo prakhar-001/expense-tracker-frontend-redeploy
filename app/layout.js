5d3d168b3a8c33930859a9f5b0fe465f0488140d
Prakhar Bhawsar <bhawsarprakhar2003@gmail.com>
2024-04-19 21:19:46 +0530

    commit -1 a

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       {/* <body className={inter.className}> */}
-      <body}>
+      <body>
         <Provider store={store}>
           {/* <div className="h-full "> */}
             {/* <Navbar/> */}
