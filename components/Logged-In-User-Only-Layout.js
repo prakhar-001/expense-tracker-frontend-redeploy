9dbdea1a98a5c261260a5c898826ec73bbae9b74
Prakhar Bhawsar <bhawsarprakhar2003@gmail.com>
2024-04-20 01:00:48 +0530

    commit - 5 c

---
+++ components/Logged-In-User-Only-Layout.js
@@ -0,0 +1,36 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+import { useSelector } from "react-redux";
+import { auth } from '@/firebase';
+
+import { useLayoutEffect } from "react";
+import { redirect } from "next/navigation";
+import toast from "react-hot-toast";
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+
+
+
+const LoggedInCustomerOnlyLayout = ({children}) => {
+
+  let sessionStatus;
+    const abc = useSelector((state) => state.userReducer.user);
+    if (abc) {
+        // console.log(abc)
+        sessionStatus = true;
+    } else {
+        sessionStatus = false;
+    }
+    useLayoutEffect(() => {
+        const session = sessionStatus;
+        if (!session) {
+        redirect("/logIn-email-pass")
+        }
+    }, []);
+
+    return (
+        // <div className='text-5xl font-semibold mx-auto'>Login To Access This Page</div>
+        <div className="h-full">{children}</div>
+    )
+}
+
+export default LoggedInCustomerOnlyLayout
